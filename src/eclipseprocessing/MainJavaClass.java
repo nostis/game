228d548ca07069014f8458a84106713ec86b1f56
Teslen <mail.wiktor.p@gmail.com>
2019-03-19 18:55:12 +0100

    Test Commit

--- src/eclipseprocessing/MainJavaClass.java
+++ src/eclipseprocessing/MainJavaClass.java
@@ -4,7 +4,7 @@ import processing.core.*;
 public class MainJavaClass {
 
 	public static void main(String[] args) {
-	    System.out.println("Hello World");
+	    System.out.println("Test");
 	    PApplet.main(new String[] {"eclipseprocessing.MainProcessingClass"});
 	}
 
